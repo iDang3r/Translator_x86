9ea13fe8fb9efdccb07a1dc63ee67f813d069151
Alex Rusin <Rusin.AI@phystech.edu>
2020-05-03 14:55:25 +0300

    Initial Commit

---
+++ Translator_x86/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Translator_x86
+//
+//  Created by Александр on 03.05.2020.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
